a765b3db455abc392d46a5ce9f595e152f9aa5a7
Florian Bieberich <florian@florians-mbp.cri.camres.org>
2017-11-08 11:56:39 +0000

    Updated import from packages

--- R/Preparation.R
+++
@@ -1,5 +0,0 @@
-#For initial quality assessment and adaptor chopping, the reads need to be supplied in a FASTA file.
-#Additionally, the UMI sourrounding adaptor sequence has to be supplied as a string element with the UMI marked as N's.
-
-UMI <- DNAString("AAGCAGTGGTATNNNNNNCAACGCAGAGT")
-fastqseq <- readFastq("file.fastq", withIds=TRUE)
