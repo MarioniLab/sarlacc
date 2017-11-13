6a9bb293c8cdb9817024a9acb9ef0500737ef317
florian0512 <florian@bieberichs.de>
2017-11-13 11:53:44 +0000

    seqAddAssign now uses fastq as input.

--- R/seqAddAssign.R
+++ R/seqAddAssign.R
@@ -1,18 +1,20 @@
-seqAddAssign <- function(fasta_file, subgroups, id_reads)
+seqAddAssign <- function(fastq_file, subgroups, id_reads)
 
 #Returns list of cluster id groups that contain the corresponding sequences
 
 {
-    names.cell4 <- read.fasta(fasta_file, as.string = TRUE, forceDNAtolower=FALSE)
-    cell4a <- readDNAStringSet(fasta_file, "fasta")
-    names(cell4a) <- names(names.cell4)
+    fastq <- readFastq("mouse_es_cell.fastq", withIds=TRUE)
+    
+    seqs <- sread(fastq)
+    names(seqs) <- ShortRead::id(fastq)
    
     #List of lists of sequence of reads that are assigned to the same cluster_id
     id_seq <- vector("list", length(subgroups))
     for (i in 1:length(subgroups)){
         reads <- id_reads[[i]]
-        id_seq[[i]] <- cell4a[reads]
+        id_seq[[i]] <- seqs[reads]
     }
     
     return(id_seq)
 }
+
