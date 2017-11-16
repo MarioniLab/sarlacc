5239e46e80e03a65150ac570f2eba2efe36d0acd
florian0512 <florian@bieberichs.de>
2017-11-16 14:56:31 +0000

    Minor changes.

--- R/clusterReads.R
+++ R/clusterReads.R
@@ -3,7 +3,7 @@ clusterReads <- function(subgroups)
 #Returns list of read names grouped by their cluster id 
 
 {
-    id_reads <- lapply(subgroups, names) #list of cluster id's containing list of reads that were assigned to the particular cluster id
+    id_reads <- lapply(subgroups, FUN="names") #list of cluster id's containing list of reads that were assigned to the particular cluster id
     
     return(id_reads)
 }
