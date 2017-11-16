1e07246d6a403584c653f13a6d3bc4cddd8b8466
florian0512 <florian@bieberichs.de>
2017-11-16 16:27:34 +0000

    Minor bug fixes.

--- R/UMIRetriev.R
+++ R/UMIRetriev.R
@@ -8,9 +8,9 @@ UMIRetriev <- function(chop_data, UMI_adaptor1_pos = NULL, UMI_adaptor2_pos = NU
     }else{
         
         UMI_seq1 <- .UMIindelextract(alignment = chop_data$adaptor1filt, UMI_pos = UMI_adaptor1_pos)
-        UMI1.collide <- .UMIcollide(UMIindelextract = UMI_seq1)
+        UMI1.collide <- .UMIcollide(UMIseq = UMI_seq1)
         
-        names(UMI1.collide) <- names()
+        names(UMI1.collide) <- names(chop_data$chopread)
         
         UMI1 <- DNAStringSet(as.character(UMI1.collide))
     }
@@ -21,11 +21,11 @@ UMIRetriev <- function(chop_data, UMI_adaptor1_pos = NULL, UMI_adaptor2_pos = NU
         
     }else{
         UMI_seq2 <- .UMIindelextract(alignment = chop_data$adaptor2filt, UMI_pos = UMI_adaptor2_pos)
-        UMI2.collide <- .UMIcollide(UMIindelextract = UMI_seq2)
+        UMI2.collide <- .UMIcollide(UMIseq = UMI_seq2)
         
-        names(UMI2.collide) <- names()
+        names(UMI2.collide) <- names(chop_data$chopread)
         
-        UMI2 <- DNAStringSet(as.character(c(UMI1.collide, UMI2.collide)))
+        UMI2 <- DNAStringSet(as.character(UMI2.collide))
     }
     
     if(is.null(UMI_adaptor1_pos)){
@@ -48,17 +48,17 @@ UMIRetriev <- function(chop_data, UMI_adaptor1_pos = NULL, UMI_adaptor2_pos = NU
             UMI_seq[[i]] <- DNAStringSet(str_sub(pattern(alignment[[i]]),UMI_start, UMI_end))
         }
         
-        return(UMI_seq = UMIseqs)
+        return(UMI_seq)
     }
     
-    .UMIcollide <- function(UMIindelextract)
+    .UMIcollide <- function(UMIseq)
     {
-        UMI.collide <- vector("list", length(UMIindelextract))
+        UMI.collide <- vector("list", length(UMIseq))
         for (i in 1:length(UMI.collide)){
-            cuts <- grep("[A-Z]", strsplit(as.character(UMIindelextract[[i]]), NULL)[[1]])
+            cuts <- grep("[A-Z]", strsplit(as.character(UMIseq[[i]]), NULL)[[1]])
             x <- vector("list", length(cuts))
             for (a in 1:length(cuts)){
-                x[a] <- strsplit(as.character(UMIindelextract[[i]]), NULL)[[1]][cuts[a]]
+                x[a] <- strsplit(as.character(UMIseq[[i]]), NULL)[[1]][cuts[a]]
             }
             UMI.collide[i] <- paste(x, collapse = "")
         } 
