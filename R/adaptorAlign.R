9dfa6cfc8a13951cf77490a0091f24e9966ef6e6
florian0512 <florian@bieberichs.de>
2017-11-20 11:24:35 +0000

    Minor changes.

--- R/adaptorAlign.R
+++ R/adaptorAlign.R
@@ -53,8 +53,8 @@ adaptorAlign <- function(adaptor1, adaptor2, reads, quality = NULL, gapOpening=1
     
     if(sum(is_reverse)!=0){
         for (i in is_reverse){
-            quality_char <- as.character(quality[[i]])
-            quality[[i]] <- BString(reverse.string(quality_char))
+            quality_char <- as.character(quality[i])
+            quality[i] <- BString(reverse.string(quality_char))
         }
         
     }
