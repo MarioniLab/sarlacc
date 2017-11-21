a57a6b7b0943189f38eb7f7655bb610d4da87692
florian0512 <florian@bieberichs.de>
2017-11-21 13:58:42 +0000

    clusterGroup and clusterReads were combined in clusterReads.

--- R/clusterReads.R
+++ R/clusterReads.R
@@ -1,13 +1,14 @@
-clusterReads <- function(map_data, subgroups, geneCount = FALSE, cluster_length = 1)
+clusterReads <- function(map_data, overlap.reads, geneCount = FALSE, cluster_length = 1)
 
 #Returns list of read names grouped by their cluster id 
 
 {
-    cluster.id <- lapply(subgroups, FUN="names") # Read names clustered in same way as reads in subgroups.
-
+    subgroups <- .clusterGrouping(overlap.reads = overlap.reads, map_data = map_data)
     
-    gene_count <- .geneCount(grange = map_data$grange, cluster.id = cluster.id)
-    cluster.seq <- .seqAssign(map_data = map_data$mapping_data, subgroups = subgroups, cluster.id = cluster.id)
+    cluster.name <- lapply(subgroups, FUN="names") # Read names clustered in same way as reads in subgroups.
+    
+    gene_count <- .geneCount(grange = map_data$grange, cluster.name = cluster.name)
+    cluster.seq <- .seqAssign(map_data = map_data$mapping_data, subgroups = subgroups, cluster.name = cluster.name)
 
     if(geneCount){
         cluster.seq <- cluster.seq[gene_count==1]
@@ -17,7 +18,9 @@ clusterReads <- function(map_data, subgroups, geneCount = FALSE, cluster_length 
     if(!is.null(cluster_length)){
         len.id <- lapply(cluster.seq, "length")
         clustered.filter <- id_seq_filt[len.id > cluster_length]
-        
+    }
+    
+    if(!is.null(cluster_length)){
         return(list(clusterReads = clustered.filter, geneCount = gene_count))
     }else{
         return(list(clusterReads = cluster.seq, geneCount = gene_count))
@@ -27,15 +30,38 @@ clusterReads <- function(map_data, subgroups, geneCount = FALSE, cluster_length 
 
 
 
-.geneCount <- function(grange, cluster.id)
+.clusterGrouping <- function(overlap.reads, map_data)
+    
+    # Returns subgroups of reads that were ordered by their cluster id.
+{
+    
+    cluster_id <- rep(0,  as.integer(names(overlap.reads)[length(overlap.reads)]))
+    
+    for (element in names(overlap.reads)) {
+        reads <- overlap.reads[[element]]
+        change.id <- cluster_id[reads]
+        change.id <- change.id[change.id!=0]
+        cluster_id[cluster_id%in%change.id] <- as.integer(element)
+        cluster_id[reads] <- as.integer(element)
+    }
+    cluster_id[cluster_id==0] <- NA
+    subgroups <- split(map_data$grange, cluster_id) #split reads in respective cluster
+    
+    return(subgroups)
+}
+
+
+
+
+.geneCount <- function(grange, cluster.name)
     #Returns vector with length of number of cluster id's containing the respective number of gene id's
 {
     
     #Subgroup gr4 by cluster id reads and store them in cluster_reads as 
     #cluster id subgroups containing respective reads and gene id in mcols
-    cluster_reads <- vector('list', length(cluster.id))
-    for (element in 1:length(cluster.id)){
-        reads <- cluster.id[[element]]
+    cluster_reads <- vector('list', length(cluster.name))
+    for (element in 1:length(cluster.name)){
+        reads <- cluster.name[[element]]
         cluster_reads[[element]] <- grange[reads]
     }
     #Check how many different gene id's are in each cluster id
@@ -47,7 +73,7 @@ clusterReads <- function(map_data, subgroups, geneCount = FALSE, cluster_length 
     return(gene_count)
 }
 
-.seqAssign <- function(map_data, subgroups, cluster.id)
+.seqAssign <- function(map_data, subgroups, cluster.name)
     #Returns list of cluster id groups that contain the corresponding sequences
 {
     seqs <- map_data$SEQ
@@ -56,7 +82,7 @@ clusterReads <- function(map_data, subgroups, geneCount = FALSE, cluster_length 
     #List of lists of sequence of reads that are assigned to the same cluster_id
     cluster.seq <- vector("list", length(subgroups))
     for (i in 1:length(subgroups)){
-        reads <- cluster.id[[i]]
+        reads <- cluster.name[[i]]
         cluster.seq[[i]] <- seqs[reads]
     }
     
