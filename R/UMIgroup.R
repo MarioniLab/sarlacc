af2505c55a19818dd4c5b4e02d63d9e43bd3e7e3
florian0512 <florian@bieberichs.de>
2017-11-22 18:19:25 +0000

    Combined UMIfilt and UMIgraphIndex in UMIgroup. UMIgroup is not taking gene counts into account. UMIs are similarly sorted, compared by their levenshtein distance and connections are used to make a graph.
    clusterReads was replaced by clusterReadsNew.
    This is now solely clustering the reads (taking a group size threshold into account) and filters the reads for each subgroup.
    By giving corresponding read ids contained in each subgroup, it prepares the application of UMIgroup.

---
+++ R/UMIgroup.R
@@ -0,0 +1,64 @@
+# UMI filt, sort, group and index.
+
+#UMI_filt=UMI1[id.nfilt]
+
+UMIgroup <- function(UMI_filt, UMI_length = 0, Levensh_threshold = 2)
+{
+    # Sorting UMI and grouping by levenshtein distance.
+    UMI_sort <- DNAStringSet(names(sort(table(UMI_filt), decreasing=TRUE)))
+    UMI_levenshtein.out <- .UMIlevenshtein(UMI_sort, levenshtein_max = UMI_length)
+
+    sortedUMI <- vector("list", length(UMI_levenshtein.out))
+    for (i in 1:length(UMI_levenshtein.out)){
+        if (length((which(UMI_levenshtein.out[[i]]<=Levensh_threshold&sort(table(UMI_filt), decreasing=TRUE)[[i]]>=2*sort(table(UMI_filt), decreasing=TRUE)-1)))!=0){
+            sortedUMI[[i]] <- rbind(i, which(UMI_levenshtein.out[[i]]<=Levensh_threshold&sort(table(UMI_filt), decreasing=TRUE)[[i]]>=2*sort(table(UMI_filt), decreasing=TRUE)-1))
+        }else{
+            
+        }
+    }
+    
+    # Connected UMIs are shown in the matrix and used to make a graph.
+    sortedUMI.bind <- do.call(cbind, sortedUMI)
+    UMI_mat_prep <- matrix(sortedUMI.bind, nc = 2, byrow = TRUE)
+    UMI_mat_prep_rev <- cbind(UMI_mat_prep[,2], UMI_mat_prep[,1])
+
+    for(i in 1:nrow(UMI_mat_prep)){
+        for(a in 1:nrow(UMI_mat_prep)){ 
+            if (sum(UMI_mat_prep[i,]==UMI_mat_prep_rev[a,]) > 1){
+                UMI_mat_prep[a,] <- c(0,0)
+            }
+        }
+    }
+    UMI_mat_prep <- UMI_mat_prep[-c(which(UMI_mat_prep[,1]==0)),]
+    UMI_graph <- make_graph(as.vector(t(UMI_mat_prep)), n = length(sortedUMI), directed = FALSE)
+
+    # Indexing the input UMIs to be able to subset the corresponding reads.
+    UMI_cluster <- split(UMI1_sort, membership(components(UMI_graph)))
+
+    UMI_index <- vector("list", length(UMI_cluster))
+    for (i in 1:length(UMI_cluster)){
+        len <- 1
+        for (a in 1:length(UMI_cluster[[i]])){
+            id <- which(UMI1_filt==UMI_cluster[[i]][a])
+            for (b in 1:length(id)){
+                UMI_index[[i]][len] <- id[b] 
+                len <- len+1
+            }
+        }
+    }
+    
+    return(UMI.id = UMI_index)
+}
+
+# Calculates levenshtein distance between UMIs.
+.UMIlevenshtein <- function(UMI_sort, levenshtein_max = 6)
+{
+    UMI_levenshtein <- vector("list", length(UMI_sort))
+    
+    for (i in 1:length(UMI_sort)){
+        UMI_levenshtein[[i]] <- stringdist(UMI_sort[i], UMI_sort)
+        UMI_levenshtein[[i]][i] <- levenshtein_max
+    } 
+    
+    return(UMI_levenshtein)
+}
---
+++ R/clusterReadsNew.R
@@ -0,0 +1,42 @@
+# For use after readOverlap.
+# Filters reads and UMIs for each subgroup.
+
+clusterReadsNew <- function(map_data, overlap.reads, GroupSize = 1)
+{
+    cluster_id <- rep(0,  as.integer(names(overlap.reads)[length(overlap.reads)]))
+    for (element in names(overlap.reads)) {
+        reads <- overlap.reads[[element]]
+        change.id <- cluster_id[reads]
+        change.id <- change.id[change.id!=0]
+        cluster_id[cluster_id%in%change.id] <- as.integer(element)
+        cluster_id[reads] <- as.integer(element)
+    }
+    cluster_id[cluster_id==0] <- NA
+    
+    cluster.name <- split(map_data, cluster_id)
+    
+    greater <- which(unlist(lapply(cluster.name, "length")) >= GroupSize)
+    
+    cluster.filt <- vector("list", length(greater))
+    id.filt <- vector("list", length(greater))
+    
+    for(i in 1:length(greater)){
+        cluster.filt[[i]] <- cluster.name[[greater[i]]]
+        id.filt[[i]] <- unique(names(unlist(cluster.name[[greater[i]]])))
+    }
+    
+    # Preparation for msa and consensus sequence.
+    # Probably better after UMIgroup.
+    # Subgrouping reads with id.
+    
+    # id.nfilt <- as.integer(id.filt[[1]])
+    # id.seq <- chop_data$reads[id.nfilt]
+}
+    
+
+    
+    
+    
+    
+    
+  
