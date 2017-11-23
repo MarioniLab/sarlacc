80cc1419b4757a8f0549a2d282e08dd8f737cc90
florian0512 <florian@bieberichs.de>
2017-11-23 17:47:07 +0000

    umiGroup works similarly as UMIgroup but compares levenshtein distance of both UMI's by adding them up and then choosing similar UMI's via the set threshold.

---
+++ R/umiGroup1.R
@@ -0,0 +1,101 @@
+
+
+
+# Function for both UMIs.
+# id.filt needs to be specified.
+# umiGroup adds the levenshtein distances of both UMI's up and then proceeds as in the initial UMIgroup function.
+
+umiGroup1 <- function(id.filt, UMI1, UMI2 = NULL, UMI_length = 12, Levensh_threshold = 2)
+{
+    if(!is.null(UMI2)){
+        UMI1_levenshtein.out <- .umiLevensh(id.filt, UMI1, UMI_length)
+        UMI2_levenshtein.out <- .umiLevensh(id.filt, UMI2, UMI_length)
+        
+        UMI.distance <- .addDistance(UMI1_levenshtein.out, UMI2_levenshtein.out)
+        Levensh_threshold <- Levensh_threshold*2
+    }else{
+        UMI.distance <- .umiLevensh(id.filt, UMI1, UMI_length)
+    }
+    
+    sortedUMI <- vector("list", length(UMI.distance))
+    for (i in 1:length(UMI.distance)){
+        if (length((which(UMI.distance[[i]]<=Levensh_threshold&sort(table(UMI_filt), decreasing=TRUE)[[i]]>=2*sort(table(UMI_filt), decreasing=TRUE)-1)))!=0){
+            sortedUMI[[i]] <- rbind(i, which(UMI.distance[[i]]<=Levensh_threshold&sort(table(UMI_filt), decreasing=TRUE)[[i]]>=2*sort(table(UMI_filt), decreasing=TRUE)-1))
+        }else{
+            sortedUMI
+        }
+    }
+    
+    # Connected UMIs are shown in the matrix and used to make a graph.
+    sortedUMI.bind <- do.call(cbind, sortedUMI)
+    if(!is.null(sortedUMI.bind)){
+        UMI_mat_prep <- matrix(sortedUMI.bind, nc = 2, byrow = TRUE)
+        UMI_mat_prep_rev <- cbind(UMI_mat_prep[,2], UMI_mat_prep[,1])
+        
+        for(i in 1:nrow(UMI_mat_prep)){
+            for(a in 1:nrow(UMI_mat_prep)){ 
+                if (sum(UMI_mat_prep[i,]==UMI_mat_prep_rev[a,]) > 1){
+                    UMI_mat_prep[a,] <- c(0,0)
+                }
+            }
+        }
+        UMI_mat_prep <- UMI_mat_prep[-c(which(UMI_mat_prep[,1]==0)),]
+        UMI_graph <- make_graph(as.vector(t(UMI_mat_prep)), n = length(sortedUMI), directed = FALSE)
+        
+        # Indexing the input UMIs to be able to subset the corresponding reads.
+        UMI_cluster <- split(UMI_sort, membership(components(UMI_graph)))
+        
+        UMI_index <- vector("list", length(UMI_cluster))
+        for (i in 1:length(UMI_cluster)){
+            len <- 1
+            for (a in 1:length(UMI_cluster[[i]])){
+                id <- which(UMI_filt==UMI_cluster[[i]][a])
+                for (b in 1:length(id)){
+                    UMI_index[[i]][len] <- id[b] 
+                    len <- len+1
+                }
+            }
+        }
+        
+    }else{
+        UMI_index <- NULL
+        
+    }
+    
+    return(UMI.id = UMI_index)
+    
+}
+
+
+.umiLevensh <- function(id.filt, UMI, UMI_length)
+{
+    id.nfilt <- as.integer(id.filt)
+    UMI_filt <- UMI[id.nfilt]
+    UMI_sort <- DNAStringSet(names(sort(table(UMI_filt), decreasing=TRUE)))
+    UMI_levenshtein.out <- .UMIlevenshtein(UMI_sort, levenshtein_max = UMI_length)
+    
+    return(UMI_levenshtein.out)
+}
+
+.addDistance <- function(UMI1_levenshtein.out, UMI2_levenshtein.out)
+{
+    UMI.distance <- vector("list", length(UMI1_levenshtein.out))
+    for(i in 1:length(UMI1_levenshtein.out)){
+        UMI.distance[[i]] <- UMI1_levenshtein.out[[i]] + UMI2_levenshtein.out[[i]]
+    }
+    
+    return(UMI.distance)
+}
+
+# Calculates levenshtein distance between UMIs.
+.UMIlevenshtein <- function(UMI_sort, levenshtein_max = 6)
+{
+    UMI_levenshtein <- vector("list", length(UMI_sort))
+    
+    for (i in 1:length(UMI_sort)){
+        UMI_levenshtein[[i]] <- stringdist(UMI_sort[i], UMI_sort)
+        UMI_levenshtein[[i]][i] <- levenshtein_max
+    } 
+    
+    return(UMI_levenshtein)
+}
