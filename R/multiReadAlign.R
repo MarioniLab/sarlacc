8e0418a451af160b44a5ae43d8ee62ee72259a9a
Aaron Lun <alun@wehi.edu.au>
2018-01-04 17:46:49 +0000

    Refactored multiReadAlign for better handling of quality-scaled input.

--- R/multiReadAlign.R
+++ R/multiReadAlign.R
@@ -1,4 +1,4 @@
-multiReadAlign <- function(reads, groups, flip=NULL, min.qual=10, ...)
+multiReadAlign <- function(reads, groups, flip=NULL, min.qual=10, keep.masked=FALSE, ...)
 # Returns a DNAStringSet of multiple sequence alignments for the sequences from each cluster id.
 # MUSCLE itself is not quality-aware, so we help it out by masking low-quality bases beforehand.
 #
@@ -13,9 +13,13 @@ multiReadAlign <- function(reads, groups, flip=NULL, min.qual=10, ...)
     if (length(groups)!=Nreads) {
         stop("length of 'reads' and 'groups' should be the same")
     }   
+
+    # Setting up some other constants.
     if (!is.null(flip) && length(flip)!=Nreads) {
         stop("length of 'flip' and 'reads' should be the same")
     }
+    has.quals <- is(reads, "QualityScaledDNAStringSet") 
+    do.mask <- !is.na(min.qual)
 
     by.group <- split(seq_len(Nreads), groups)
     msalign <- vector("list", length(by.group))
@@ -33,24 +37,41 @@ multiReadAlign <- function(reads, groups, flip=NULL, min.qual=10, ...)
             cur.reads[curflip] <- reverseComplement(cur.reads[curflip])
         }
 
-        cur.reads <- .mask_bad_bases(cur.reads, threshold=min.qual)
-        cur.align <- muscle(cur.reads, ..., quiet=TRUE)
+        # Performing the MSA on potentially masked reads.
+        if (has.quals) {
+            if (do.mask) { 
+                to.use <- .mask_bad_bases(cur.reads, threshold=min.qual)
+            } else {
+                to.use <- as(cur.reads, "DNAStringSet")
+            }
+        } else {
+            to.use <- cur.reads
+        }
+        cur.align <- muscle(to.use, ..., quiet=TRUE)
+        cur.align <- unmasked(cur.align)
 
+        # Unmasking the bases in the alignments.
+        if (has.quals && do.mask && !keep.masked) {
+            cur.align <- .unmask_bases(cur.align, to.use) 
+        }
+
+        # Storing the quality strings in the elementwise-metadata.
+        if (has.quals) {
+            elementMetadata(cur.align)$quality <- quality(cur.reads)
+        }
         msalign[[g]] <- cur.align
     }
     return(msalign)
 }
 
 .mask_bad_bases <- function(incoming, threshold) {
-    if (!is(incoming, "QualityScaledDNAStringSet")) {
-        return(incoming)
-    } else if (is.na(threshold)) { 
-        return(as(incoming, "DNAStringSet"))        
-    } else {
-        enc <- encoding(quality(incoming))
-        lowerbound <- names(enc)[min(which(enc>=threshold))]
-        out <- .Call(cxx_mask_bad_bases, incoming, quality(incoming), lowerbound)
-        return(DNAStringSet(out))
-    }
+    enc <- encoding(quality(incoming))
+    lowerbound <- names(enc)[min(which(enc>=threshold))]
+    out <- .Call(cxx_mask_bad_bases, incoming, quality(incoming), lowerbound)
+    return(DNAStringSet(out))
 }
 
+.unmask_bases <- function(alignments, originals) {
+    out <- .Call(cxx_unmask_bases, alignments, originals)
+    return(DNAStringSet(out))
+}
