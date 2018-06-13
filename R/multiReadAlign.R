df3d985ce7065490add978a587d0fba0406257b6
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-06-13 16:57:14 +0100

    Modified multiReadAlign to work with and return character strings.

--- R/multiReadAlign.R
+++ R/multiReadAlign.R
@@ -1,5 +1,8 @@
 #' @export
 #' @importFrom BiocParallel bplapply SerialParam
+#' @importClassesFrom Biostrings QualityScaledDNAStringSet
+#' @importFrom Biostrings quality encoding
+#' @importFrom methods is
 multiReadAlign <- function(reads, groups, min.qual=10, keep.masked=FALSE, ..., BPPARAM=SerialParam())
 # Returns a DNAStringSet of multiple sequence alignments for the sequences from each cluster id.
 # MUSCLE itself is not quality-aware, so we help it out by masking low-quality bases beforehand.
@@ -20,80 +23,62 @@ multiReadAlign <- function(reads, groups, min.qual=10, keep.masked=FALSE, ..., B
         by.group <- split(seq_len(Nreads), groups)
     }
 
-    reads.by.group <- lapply(by.group, FUN=function(idx) reads[idx]) # reduce memory allocation within child processes. 
-    bplapply(reads.by.group, FUN=.internal_msa, min.qual=min.qual, keep.masked=keep.masked, ..., BPPARAM=BPPARAM)
-}
-
-#' @importClassesFrom Biostrings DNAStringSet QualityScaledDNAStringSet
-#' @importFrom Biostrings unmasked quality
-#' @importFrom S4Vectors elementMetadata<-
-#' @importFrom methods is as
-#' @importFrom muscle muscle
-.internal_msa <- function(cur.reads, min.qual, keep.masked, ...) {
-    has.quals <- is(cur.reads, "QualityScaledDNAStringSet")
+    # Setting quality-related parameters.
+    has.quals <- is(reads, "QualityScaledDNAStringSet")
     do.mask <- !is.na(min.qual)
-
-    if (length(cur.reads)==1L) {
-        if (has.quals) {
-            if (do.mask && keep.masked) {
-                cur.align <- .mask_bad_bases(cur.reads, threshold=min.qual)
-            } else {
-                cur.align <- as(cur.reads, "DNAStringSet")
-            }
+    if (has.quals) {
+        if (is.na(min.qual)) { 
+            enc <- encoding(quality(reads))
+            lowerbound <- names(enc)[min(which(enc>=min.qual))] 
         } else {
-            cur.align <- cur.reads
-        }
-    } else {
-        # Performing the MSA on potentially masked reads.
-        if (has.quals) {
-            if (do.mask) {
-                to.use <- .mask_bad_bases(cur.reads, threshold=min.qual)
-            } else {
-                to.use <- as(cur.reads, "DNAStringSet")
-            }
-        } else {
-            to.use <- cur.reads
-        }
-        cur.align <- muscle(to.use, ..., quiet=TRUE)
-        cur.align <- unmasked(cur.align)
-
-        # Unmasking the bases in the alignments.
-        if (has.quals && do.mask && !keep.masked) {
-            cur.align <- .unmask_bases(cur.align, cur.reads)
+            lowerbound <- NA_character_
         }
     }
 
-    # Storing the quality strings in the elementwise-metadata.
+    # Converting everything to a string, which is faster to work with.
+    all.masked <- all.qual <- NULL
+    all.reads <- as.character(reads)
     if (has.quals) {
-        elementMetadata(cur.align)$quality <- quality(cur.reads)
+        all.qual <- as.character(quality(reads))
+        if (do.mask) {
+            all.masked <- .Call(cxx_mask_bad_bases, all.reads, all.qual, min.enc)
+        }
+    } 
+
+    # Running the multiple sequence alignment across multiple cores.
+    multi.res <- bplapply(by.group, FUN=.internal_msa, reads=all.reads, qualities=all.qual, masked=all.masked, 
+        ..., keep.masked=keep.masked, BPPARAM=BPPARAM)
+
+    # Collating the results into a single DF.
+    out <- new("DataFrame", nrows=length(by.group))
+    out$alignments <- multi.res 
+    if (has.quals) {
+        out$qualities <- lapply(by.group, function(x) all.qual[idx]) 
+    }
+    rownames(out) <- names(by.group)
+    return(out)
+}
+
+#' @importFrom Biostrings unmasked
+#' @importFrom muscle muscle
+.internal_msa <- function(indices, reads, qualities, masked, keep.masked, ...) {
+    do.mask <- !is.null(masked)
+    if (length(indices)==1L) { 
+        if (keep.masked) {
+            return(masked[indices])
+        } else {
+            return(reads[indices])
+        }
+    }
+
+    to.use <- if (do.mask) masked[indices] else reads[indices]
+    cur.align <- muscle(DNAStringSet(to.use), ..., quiet=TRUE)
+    cur.align <- unmasked(cur.align)
+    
+    if (do.mask && !keep.masked) {
+        cur.align <- .Call(cxx_unmask_bases, cur.align, reads[indices])
+    } else {
+        cur.align <- as.character(cur.align)
     }
     cur.align
 }
-
-#' @importFrom Biostrings encoding quality DNAStringSet
-.mask_bad_bases <- function(incoming, threshold) {
-    enc <- encoding(quality(incoming))
-    lowerbound <- names(enc)[min(which(enc>=threshold))]
-    out <- .Call(cxx_mask_bad_bases, incoming, quality(incoming), lowerbound)
-    return(DNAStringSet(out))
-}
-
-#' @importFrom Biostrings DNAStringSet
-.unmask_bases <- function(alignments, originals) {
-    out <- .Call(cxx_unmask_bases, alignments, originals)
-    return(DNAStringSet(out))
-}
-
-#' @importFrom Biostrings QualityScaledDNAStringSet DNAStringSet
-#' @importFrom methods is
-.safe_masker <- function(incoming, threshold) {
-    if (is(incoming, "QualityScaledDNAStringSet") && !is.na(threshold)) {
-        return(.mask_bad_bases(incoming, threshold))
-    } else {
-        return(as(incoming, "DNAStringSet"))
-    }
-}
-
-
-
-
