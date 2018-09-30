96ab49ba060755ddcc934813aad24af3f1fa8071
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-09-30 16:50:19 +0100

    Switched to a streaming model in getAdaptorThresholds.

--- R/getAdaptorThresholds.R
+++ R/getAdaptorThresholds.R
@@ -2,46 +2,64 @@
 #' @importFrom S4Vectors metadata
 #' @importClassesFrom Biostrings QualityScaledDNAStringSet
 #' @importFrom BiocParallel SerialParam
-getAdaptorThresholds <- function(aligned, error=0.01, BPPARAM=SerialParam())
+#' @importFrom ShortRead FastqStreamer yield
+getAdaptorThresholds <- function(aligned, error=0.01, block.size=1e8, BPPARAM=SerialParam())
 # Scrambles the input sequence and performs the same thing as adaptorAlign but with a scrambled input. 
 # Identifies the score threshold for the adaptors that achieves the specified error rate.
 #
 # written by Aaron Lun
 # created 10 March 2018
 {
-    reads <- aligned$reads
     adaptor1 <- metadata(aligned$adaptor1)$sequence
     adaptor2 <- metadata(aligned$adaptor2)$sequence
-    tolerance <- metadata(aligned$adaptor1)$tolerance
+
     go <- metadata(aligned$adaptor1)$gapOpening 
     ge <- metadata(aligned$adaptor1)$gapExtension
-    ma <- metadata(aligned$adaptor1)$match
-    mm <- metadata(aligned$adaptor1)$mismatch
-
-    # Scrambling the start and end of the read sequences.
-    reads.out <- .get_front_and_back(reads, tolerance)
-    reads.start <- reads.out$front
-    reads.end <- reads.out$back
-    
-    has.quality <- is(reads, "QualityScaledDNAStringSet")
-    scrambled.start <- .scramble_input(reads.start, has.quality)
-    scrambled.end <- .scramble_input(reads.end, has.quality)
-
-    # Computing alignment scores to the adaptors.
-    all.args <- .setup_alignment_args(has.quality, go, ge, ma, mm)
+    all.args <- .setup_alignment_args(TRUE, go, ge)
     all.args$BPPARAM <- BPPARAM
     all.args$scoreOnly <- TRUE
 
-    scrambled_start <- do.call(.bplalign, c(list(adaptor=adaptor1, reads=scrambled.start), all.args))
-    scrambled_end <- do.call(.bplalign, c(list(adaptor=adaptor2, reads=scrambled.end), all.args))
-    scrambled_revcomp_start <- do.call(.bplalign, c(list(adaptor=adaptor1, reads=scrambled.end), all.args))
-    scrambled_revcomp_end <- do.call(.bplalign, c(list(adaptor=adaptor2, reads=scrambled.start), all.args))
-    is.reverse <- .resolve_strand(scrambled_start, scrambled_end, scrambled_revcomp_start, scrambled_revcomp_end)$reversed
+    tolerance <- metadata(aligned)$tolerance
+    filepath <- metadata(aligned)$filepath
+    qual.type <- metadata(aligned)$qual.type
+    qual.class <- .qual2class(qual.type)
 
-    scram.score1 <- ifelse(is.reverse, scrambled_revcomp_start, scrambled_start)
-    scram.score2 <- ifelse(is.reverse, scrambled_revcomp_end, scrambled_end)
-    real.score1 <- aligned$adaptor1$score
-    real.score2 <- aligned$adaptor2$score
+    # Looping through the files.
+    fhandle <- FastqStreamer(filepath, readerBlockSize=block.size)
+    on.exit(close(fhandle))
+    scram.score1 <- scram.score2 <- used.names <- list()
+    counter <- 1L
+
+    while (length(fq <- yield(fhandle))) {
+        reads <- .FASTQ2QSDS(fq, qual.class)
+        reads <- reads[names(reads) %in% rownames(aligned)]
+        used.names[[counter]] <- names(reads)
+
+        # Scrambling the start and end of the read sequences.
+        reads.out <- .get_front_and_back(reads, tolerance)
+        reads.start <- reads.out$front
+        reads.end <- reads.out$back
+
+        scrambled.start <- .scramble_input(reads.start, TRUE)
+        scrambled.end <- .scramble_input(reads.end, TRUE)
+
+        # Computing alignment scores to the adaptors.
+        scrambled_start <- do.call(.bplalign, c(list(adaptor=adaptor1, reads=scrambled.start), all.args))
+        scrambled_end <- do.call(.bplalign, c(list(adaptor=adaptor2, reads=scrambled.end), all.args))
+        scrambled_revcomp_start <- do.call(.bplalign, c(list(adaptor=adaptor1, reads=scrambled.end), all.args))
+        scrambled_revcomp_end <- do.call(.bplalign, c(list(adaptor=adaptor2, reads=scrambled.start), all.args))
+
+        is.reverse <- .resolve_strand(scrambled_start, scrambled_end, scrambled_revcomp_start, scrambled_revcomp_end)$reversed
+        scram.score1[[counter]] <- ifelse(is.reverse, scrambled_revcomp_start, scrambled_start)
+        scram.score2[[counter]] <- ifelse(is.reverse, scrambled_revcomp_end, scrambled_end)
+        counter <- counter + 1L
+    }
+
+    scram.score1 <- unlist(scram.score1)
+    scram.score2 <- unlist(scram.score2)
+    m <- match(unlist(used.names), rownames(aligned))
+    real.score1 <- aligned$adaptor1$score[m]
+    real.score2 <- aligned$adaptor2$score[m]
 
     list(
         threshold1=.compute_threshold(real.score1, scram.score1, error),
