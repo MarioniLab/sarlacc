d3492cd8ad3af4ac9c6b7990e1f6346d425e7561
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-10-01 10:37:49 +0100

    Added function to realize reads from the delayed ops.

---
+++ R/realizeReads.R
@@ -0,0 +1,46 @@
+#' @export
+#' @importFrom S4Vectors metadata
+#' @importFrom ShortRead FastqStreamer yield
+#' @importFrom Biostrings reverseComplement subseq
+realizeReads <- function(aligned, block.size=1e8, trim=TRUE) 
+# Converts conceptual reads into actual reads.
+#
+# written by Aaron Lun
+# created 1 October 2018
+{
+    filepath <- metadata(aligned)$filepath
+    qual.type <- metadata(aligned)$qual.type
+    qual.class <- .qual2class(qual.type)
+
+    fhandle <- FastqStreamer(filepath, readerBlockSize=block.size)
+    on.exit(close(fhandle))
+    all.reads <- list()
+    counter <- 1L
+
+    while (length(fq <- yield(fhandle))) {
+        reads <- .FASTQ2QSDS(fq, qual.class)
+        reads <- reads[names(reads) %in% rownames(aligned)]
+        all.reads[[counter]] <- reads
+        counter <- counter+1L
+    }
+
+    all.reads <- do.call(c, all.reads)
+    m <- match(rownames(aligned), names(all.reads))
+    if (any(is.na(m))) {
+        stop("read names in 'aligned' not present in FASTQ file")
+    }
+    all.reads <- all.reads[m]
+
+    # Applying reverse-complementing and trimming.
+    all.reads[aligned$reversed] <- reverseComplement(all.reads[aligned$reversed])
+
+    if (trim) {
+        if (!is.null(aligned$trim.start)) {
+            all.reads <- subseq(all.reads, aligned$trim.start, aligned$trim.end)
+        } else { 
+            warning("no 'trim.start' detected, run 'filterReads' first")
+        }
+    }
+
+    all.reads
+}
