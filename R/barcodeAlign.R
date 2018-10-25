a4130e45977700c6d181796933ac9c51e1140c6c
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-10-25 16:02:36 +0100

    Updated barcodeAlign code to use new C++ code.

--- R/barcodeAlign.R
+++ R/barcodeAlign.R
@@ -1,34 +1,26 @@
 #' @export
-#' @importFrom S4Vectors metadata 
-#' @importFrom BiocParallel SerialParam
-barcodeAlign <- function(align.stats, barcodes, position, BPPARAM=SerialParam())
+#' @importFrom S4Vectors metadata metadata<- DataFrame
+#' @importFrom BiocParallel SerialParam bpstart bpstop bpisup bpmapply
+barcodeAlign <- function(sequences, barcodes, gapOpening=5, gapExtension=1, BPPARAM=SerialParam())
 # Pulls out the barcodes, aligns them against all possible options,
 # and reports the results.
 #
 # written by Aaron Lun
 # created 19 September 2018
 {
-    extracted <- umiExtract(align.stats, position=position)
-    bout <- .align_to_barcodes(extracted, barcodes, metadata(align.stats), BPPARAM)
-    output <- DataFrame(sequence=extracted, bout, row.names=rownames(align.stats))
-    metadata(output) <- c(metadata(align.stats)[c("gapOpening", "gapExtension", "match", "mismatch")], list(barcodes=barcodes))
-    output
-}
-
-#' @importFrom methods is
-#' @importClassesFrom Biostrings QualityScaledDNAStringSet
-#' @importFrom S4Vectors DataFrame metadata<-
-.align_to_barcodes <- function(sequences, barcodes, param.list, BPPARAM) {
-    all.args <- .setup_alignment_args(TRUE, param.list$gapOpening, param.list$gapExtension, type="global")
-    all.args$scoreOnly <- TRUE
     by.core <- .parallelize(sequences, BPPARAM)
-
     current.score <- next.best <- rep(-Inf, length(sequences))
     current.id <- rep(NA_integer_, length(sequences))
+
+    if (!bpisup(BPPARAM)) {
+        bpstart(BPPARAM)
+        on.exit(bpstop(BPPARAM), add=TRUE)
+    }
+
     for (b in seq_along(barcodes)) {
-        current <- .assign_qualities(barcodes[b])
-        scores <- bpmapply(FUN=pairwiseAlignment, pattern=by.core, 
-            MoreArgs=c(list(subject=current), all.args),
+        current <- toupper(as.character(barcodes[b]))
+        scores <- bpmapply(FUN=.align_BA_internal, barcodes=by.core, 
+            MoreArgs=list(reference=barcodes[b], gap.opening=gapOpening, gap.extension=gapExtension),
             BPPARAM=BPPARAM, SIMPLIFY=FALSE, USE.NAMES=FALSE)
         scores <- unlist(scores)
 
@@ -42,5 +34,14 @@ barcodeAlign <- function(align.stats, barcodes, position, BPPARAM=SerialParam())
         next.best[second.keep] <- scores[second.keep]
     }
 
-    list(barcode=current.id, score=current.score, gap=current.score - next.best)
+    output <- DataFrame(barcode=current.id, score=current.score, gap=current.score - next.best)
+    metadata(output) <- list(gapOpening=gapOpening, gapExtension=gapExtension, barcodes=barcodes)
+    output
+}
+
+#' @importFrom Biostrings quality
+.align_BA_internal <- function(barcodes, reference, gap.opening, gap.extension) {
+    quals <- quality(barcodes)
+    .Call(cxx_barcode_align, barcodes, quals, .create_encoding_vector(quals), 
+            gap.opening, gap.extension, reference)
 }
