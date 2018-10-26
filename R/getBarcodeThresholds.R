e00b68703ef163d975fb82273bdf0ee2428d6e80
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-10-26 11:10:13 +0100

    Simplified getBarcodeThresholds to use outlier-based threshold.

--- R/getBarcodeThresholds.R
+++ R/getBarcodeThresholds.R
@@ -1,20 +1,15 @@
 #' @export
-#' @importFrom S4Vectors metadata
-#' @importClassesFrom Biostrings QualityScaledDNAStringSet
-#' @importFrom BiocParallel SerialParam
-#' @importFrom methods is
-getBarcodeThresholds <- function(baligned, error=0.01, BPPARAM=SerialParam())
+#' @importFrom IRanges median mad
+getBarcodeThresholds <- function(baligned, nmads=3)
 # Computes thresholds for the barcode alignments,
 # by comparison to scrambled sequences from the barcode segment of the read.
 # 
 # written by Aaron Lun
 # created 27 September 2018
 {
-    seq <- baligned$sequence
-    scrambled <- .scramble_input(seq, is(seq, "QualityScaledDNAStringSet"))
-    bscores <- .align_to_barcodes(scrambled, metadata(baligned)$barcodes, metadata(baligned), BPPARAM)
-
-    on.score <- .compute_threshold(baligned$score, bscores$score, error)
-    on.diff <- .compute_threshold(baligned$gap, bscores$gap, error)
-    c(score=on.score, gap=on.diff)
+    med.s <- median(baligned$score)
+    mad.s <- mad(baligned$score, center=med.s)
+    med.g <- median(baligned$gap)
+    mad.g <- mad(baligned$gap, center=med.g)
+    c(score=med.s - mad.s*nmads, gap=med.g - mad.g*nmads)
 }
