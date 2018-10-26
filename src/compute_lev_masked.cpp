5c5418b5fc376533e81a8e98f57f7edb1646eb1e
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-10-26 11:45:03 +0100

    Updated C++ levenshtein distance calculator.

--- src/compute_lev_masked.cpp
+++ src/compute_lev_masked.cpp
@@ -1,15 +1,16 @@
 #include "sarlacc.h"
+#include "DNA_input.h"
 
 /* This function computes a masked version of the Levenshtein distance,
  * where N's always contribute a mismatch no matter what they are matched to.
  * In effect, they are treated as 'missing' values.
  */
 
-SEXP compute_lev_masked(SEXP seqs) {
+SEXP compute_lev_masked(SEXP sequences) {
     BEGIN_RCPP
 
-    auto seq=hold_XStringSet(seqs);
-    const size_t nseq=get_length_from_XStringSet_holder(&seq);
+    auto seqs=process_DNA_input(sequences);
+    const size_t nseq=seqs->size();
     Rcpp::NumericVector output((nseq-1)*nseq/2);
     auto oIt=output.begin();
 
@@ -19,28 +20,30 @@ SEXP compute_lev_masked(SEXP seqs) {
     prev_col.reserve(50);
 
     for (size_t i=0; i<nseq; ++i) {
-        auto iseq=get_elt_from_XStringSet_holder(&seq, i);
-        const char* istr=iseq.ptr;
-        const size_t ilen=iseq.length;
+        auto iseq=seqs->get(i);
+        const char* istr=iseq.first;
+        const size_t ilen=iseq.second;
 
         if (prev_col.size() < ilen) {
             col.resize(ilen + 1);
             prev_col.resize(ilen + 1);
         }
 
+        auto altseqs=process_DNA_input(sequences);
+
         for (size_t j=i+1; j<nseq; ++j) {
-            auto jseq=get_elt_from_XStringSet_holder(&seq, j);
-            const char* jstr=jseq.ptr;
-            const size_t jlen=jseq.length;
+            auto jseq=altseqs->get(j);
+            const char* jstr=jseq.first;
+            const size_t jlen=jseq.second;
 
             // Computing the levenshtein distance.
             std::iota(prev_col.begin(), prev_col.end(), 0);
             for (size_t jx=0; jx<jlen; ++jx) {
                 col[0]=jx+1;
-                const char jbase=DNAdecode(jstr[jx]);
+                const char jbase=jstr[jx];
 
                 for (size_t ix=0; ix<ilen; ++ix) {
-                    const char ibase=DNAdecode(istr[ix]);
+                    const char ibase=istr[ix];
                     const double match_score = (jbase=='N' || ibase=='N' ? 0.5 : (jbase==ibase ? 0 : 1));
                     col[ix+1] = std::min({ prev_col[ix+1] + 1, col[ix] + 1, prev_col[ix] + match_score });
                 }
