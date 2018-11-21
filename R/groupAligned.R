f9b5be780b260af90222eccb6a0720b508a6baaf
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-11-21 15:32:58 +0000

    Used simpler algorithms for alignment grouping.

--- R/groupAligned.R
+++ R/groupAligned.R
@@ -1,58 +1,61 @@
 #' @export
-#' @importFrom IRanges findOverlaps
+#' @importFrom IRanges findOverlaps coverage
 #' @importFrom S4Vectors queryHits subjectHits
-#' @importFrom igraph make_graph cluster_fast_greedy
-#' @importFrom BiocGenerics width intersect
-#' @importFrom methods is
+#' @importFrom BiocGenerics width strand<-
+#' @importFrom methods is as
 #' @importClassesFrom GenomicRanges GenomicRanges
-groupAligned <- function(locations, threshold=0.8, cluster.fun=cluster_fast_greedy) 
-# Groups alignments based on their position and overlap.
+#' @importFrom GenomeInfoDb seqnames
+groupAlignedGenome <- function(locations) 
+# Groups alignments based on their genomic coordinates.
 #
 # written by Aaron Lun
 # created 22 November 2018
 {
     mapped <- seqnames(locations)!="*"
-    olap <- findOverlaps(locations[mapped], ignore.strand=FALSE)
-    all.q <- queryHits(olap)
-    all.s <- subjectHits(olap)
+    pcov <- coverage(locations[mapped & strand(locations)=="+"])
+    ncov <- coverage(locations[mapped & strand(locations)=="-"])
 
-    # Adjust for changing of indices when subsetting a GR (but not GRL).
-    if (is(locations, "GenomicRanges")) {
-        mapped <- which(mapped)
-        all.q <- mapped[all.q]
-        all.s <- mapped[all.s]
-    }
+    # Creating 'standard' intervals to overlap against.
+    pstandards <- as(slice(pcov, lower=1), "GenomicRanges")
+    nstandards <- as(slice(ncov, lower=1), "GenomicRanges")
+    strand(pstandards) <- "*"
+    strand(nstandards) <- "-"
+    standards <- c(pstandards, nstandards)
 
-    # Remove redundant pairs, unmapped reads in self-overlap.
-    keep <- all.q < all.s
-    all.q <- all.q[keep]
-    all.s <- all.s[keep]
-
-    # Checking the span of the overlaps.
-    left <- locations[all.q]
-    right <- locations[all.s]
-    overlapping <- intersect(left, right)
-
-    olap.width <- vapply(width(overlapping), sum, FUN.VALUE=0L)
-    left.width <- vapply(width(left), sum, FUN.VALUE=0L)
-    right.width <- vapply(width(right), sum, FUN.VALUE=0L)
-    olap.prop <- olap.width / pmax(left.width, right.width)
-
-    keep <- olap.prop >= threshold
-    all.q <- all.q[keep]
-    all.s <- all.s[keep]
-
-    # Creating the graph and clustering.
-    edges <- rbind(all.q, all.s)
-    g <- make_graph(edges, n=length(locations), directed=FALSE)
-    out <- cluster.fun(g)$membership
-
-    if (is(locations, "GenomicRanges")) {
-        out[-mapped] <- NA_integer_
-    } else {
-        out[!any(mapped)] <- NA_integer_
-    }
-
-    names(out) <- names(locations)
-    out
+    # All mapped elements should overlap with at least one standard,
+    # and exactly one if the elements do not have multiple entries.
+    olap <- findOverlaps(locations, standards, ignore.strand=FALSE, select="first")
+    .clear_unmapped(olap, locations, mapped)
 }
+
+#' @export
+#' @importFrom S4Vectors head
+#' @importFrom BiocGenerics strand
+#' @importFrom GenomeInfoDb seqnames
+groupAlignedTrans <- function(locations) 
+# Groups alignments based on the reference sequence name.
+#
+# written by Aaron Lun
+# created 22 November 2018
+{
+    mapped <- seqnames(locations)!="*"
+    first <- c(1L, 1L+cumsum(head(lengths(locations), -1)))
+    by.rname <- unlist(seqnames(locations), use.names=FALSE)[first]
+    by.strand <- unlist(strand(locations), use.names=FALSE)[first]
+    .clear_unmapped(
+        as.integer(factor(paste0(by.rname, ":", by.strand))), 
+        locations, mapped)
+}
+
+#' @importFrom methods is
+#' @importClassesFrom GenomicRanges GenomicRanges
+.clear_unmapped <- function(grouping, locations, mapped) {
+    if (is(locations, "GenomicRanges")) {
+        grouping[-mapped] <- NA_integer_
+    } else {
+        grouping[!any(mapped)] <- NA_integer_
+    }
+    names(grouping) <- names(locations)
+    grouping
+}
+
