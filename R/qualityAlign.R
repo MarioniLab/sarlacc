c497dc177c37825a35874c04d33f16b43599d880
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2019-02-24 17:22:55 +0000

    Add edit.only output option to qualityAlign.

--- R/qualityAlign.R
+++ R/qualityAlign.R
@@ -1,7 +1,7 @@
 #' @export
 #' @importFrom S4Vectors DataFrame
 #' @importFrom BiocParallel SerialParam bpstart bpstop bpisup bpmapply
-qualityAlign <- function(sequences, reference, gapOpening=5, gapExtension=1, BPPARAM=SerialParam())
+qualityAlign <- function(sequences, reference, gapOpening=5, gapExtension=1, edit.only=FALSE, BPPARAM=SerialParam())
 {
     by.core <- .parallelize(sequences, BPPARAM)
     if (!bpisup(BPPARAM)) {
@@ -11,19 +11,24 @@ qualityAlign <- function(sequences, reference, gapOpening=5, gapExtension=1, BPP
 
 	ref <- toupper(as.character(reference))
     out <- bpmapply(FUN=.align_general_internal, sequences=by.core, 
-        MoreArgs=list(reference=ref, gap.opening=gapOpening, gap.extension=gapExtension),
+        MoreArgs=list(reference=ref, gap.opening=gapOpening, gap.extension=gapExtension, edit.only),
         BPPARAM=BPPARAM, SIMPLIFY=FALSE, USE.NAMES=FALSE)
 
     output <- mapply(out, FUN=c)
-    names(output) <- c("score", "reference", "query")
+    if (edit.only) {
+        names(output) <- c("score", "edit")
+    } else {
+        names(output) <- c("score", "edit", "reference", "query")
+    }
+
     output <- do.call(DataFrame, output)
     metadata(output) <- list(gapOpening=gapOpening, gapExtension=gapExtension, reference=reference)
     output
 }
 
 #' @importFrom Biostrings quality
-.align_general_internal <- function(sequences, reference, gap.opening, gap.extension) {
+.align_general_internal <- function(sequences, reference, gap.opening, gap.extension, edit.only) {
     quals <- quality(sequences)
     .Call(cxx_general_align, sequences, quals, .create_encoding_vector(quals), 
-            gap.opening, gap.extension, reference)
+        gap.opening, gap.extension, reference, edit.only)
 }
