c20f63869a2e416bb94e959727c2d6c93e2ab788
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2019-02-25 11:16:48 +0000

    Clean up Imports and Depends.

--- R/namespace.R
+++ R/namespace.R
@@ -4,11 +4,9 @@
 #' @import methods
 #' @import S4Vectors
 #' @import GenomicRanges
-#' @import BiocParallel
 #' @importFrom Rcpp sourceCpp
 #' @useDynLib sarlacc, .registration=TRUE, .fixes="cxx_"
 NULL
 
 # Note: need XVector, IRanges and methods "Imports" for the Biostrings C API.
 # Note: need Biostrings, S4Vectors and GenomicRanges "Depends", as these are returned to the user.
-# Note: need BiocParallel "Depends", as these are directly set by the user.
